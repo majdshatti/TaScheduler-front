cbeeb2e0f90287e58cdeb25b63d60090e152fb7e
majdshatti <majdshatti8@gmail.com>
2022-08-05 23:33:46 +0300

    Flex box components tested

--- src/feature/Home/index.tsx
+++ src/feature/Home/index.tsx
@@ -1,5 +1,7 @@
 // Material UI
-import { Container, Typography } from "@mui/material";
+import { Container, Grid, Typography, Button } from "@mui/material";
+
+import FlexColBox from "../../components/box/col-box";
 // Custom Styles
 import "./home.styles.scss";
 
@@ -11,10 +13,31 @@ const Home = () => {
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
+          flexDirection: "column",
           height: "100vh",
+          textAlign: "center",
         }}
       >
-        <Typography variant="h1">Welcome, to TaScheduler</Typography>
+        <FlexColBox gap={5} alignItems="center">
+          <Typography variant="h1">Welcome, to TaScheduler</Typography>
+
+          <FlexColBox gap={3} sx={{ width: "70%", textAlign: "center" }}>
+            <Typography
+              variant="body1"
+              fontSize="1.3rem"
+              sx={{ textAlign: "center" }}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Asperiores suscipit ab expedita reprehenderit pariatur
+              repudiandae? Reprehenderit modi minima ab quam quia! Animi debitis
+              ad eum porro! Fugit nesciunt saepe quibusdam!
+            </Typography>
+
+            <Button variant="outlined" size="large">
+              Get Started
+            </Button>
+          </FlexColBox>
+        </FlexColBox>
       </Container>
     </div>
   );
